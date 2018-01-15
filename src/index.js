b83c035dc459fc7ae71d1b05c2bf16b376766a8e
felipesuero <felipe@motivislearning.com>
2018-01-14 19:33:25 -0500

    posts

--- src/index.js
+++ src/index.js
@@ -4,26 +4,4 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-class Post extends React.Component{
-	render(){
-		return (
-			<div>
-				<br /> 
-				<h1> Post </h1> 
-				<p> {this.props.post} </p>
-			</div>)
-	}
-}
-
-function PostArray(){
-	return (
-    <div>
-      <Post post="Sara" />
-      <Post post="Cahal" />
-      <Post post="Edite" />
-    </div>
-  );
-}
-
-const element = < PostArray />
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
